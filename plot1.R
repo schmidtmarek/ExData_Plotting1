9984a86c663f3246efa60497214b808c1bd457a3
Marek Schmidt <marks73@gmail.com>
2014-10-13 01:34:31 +0200

    plot1

---
+++ plot1.R
@@ -0,0 +1,15 @@
+
+setwd("c:/Users/Marek/Desktop/R/")
+md <- read.csv("household_power_consumption.txt", sep=";");
+md$Date <- as.Date(strptime(md$Date,"%d/%m/%Y"));
+md1 <- md[md$Date == as.Date(strptime("2007-02-01", "%Y-%m-%d")) | 
+                  md$Date == as.Date(strptime("2007-02-02", "%Y-%m-%d")),];
+
+hist(as.numeric(md1$Global_active_power), col="red", xlab="Global Active Power (kilowatts)", 
+     main="Global Active Power");
+
+mf <- png(filename="plot1.png", width = 480, height = 480);
+with (mf, hist(as.numeric(md1$Global_active_power), col="red", xlab="Global Active Power (kilowatts)", 
+        main="Global Active Power"));
+dev.off();
+
